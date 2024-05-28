8aa7b27649e2a7c5538b8155306a6902da5c614b
Andriy Sotnyk <lodochnikkot@gmail.com>
2024-05-28 23:17:29 +0300

    add some configurations

---
+++ scripts/deploy-gateway.sh
@@ -0,0 +1,2 @@
+kubectl apply -f ../gateway-service/k8s/gateway-deployment.yaml
+kubectl apply -f ../gateway-service/k8s/gateway-service.yaml
---
+++ scripts/deploy-users-service.sh
@@ -0,0 +1,4 @@
+kubectl apply -f ../users-db/users-db-pv.yaml
+kubectl apply -f ../users-db/users-db-claim.yaml
+kubectl apply -f ../users-db/users-db-deployment.yaml
+kubectl apply -f ../users-db/user-db-service.yaml
