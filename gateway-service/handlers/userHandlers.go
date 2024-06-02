b04d709e6d7363bd877957836fbe3a7c6cc9f237
Andriy Sotnyk <lodochnikkot@gmail.com>
2024-06-02 21:51:48 +0300

    remove handlers folder, moved user service domain into config module

--- gateway-service/handlers/userHandlers.go
+++
@@ -1,8 +0,0 @@
-package handlers
-
-import "github.com/gofiber/fiber/v2"
-
-func CreateUser(c *fiber.Ctx) error {
-	// Todo: proxy request to user service
-	return nil
-}
--- gateway-service/router/userRouters.go
+++ gateway-service/router/userRouters.go
@@ -2,17 +2,17 @@ package router
 
 import (
 	"fmt"
+	"little-storage/gateway-service/config"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/proxy"
 )
 
 const (
-	USER_SERVICE      = "http://user:4000/"
 	USER_CREATE_ROUTE = "user"
 )
 
 func setupUserRoutes(a *fiber.App) {
 	USER := a.Group("user")
-	USER.Post("/", proxy.Forward(fmt.Sprintf("%s%s", USER_SERVICE, USER_CREATE_ROUTE)))
+	USER.Post("/", proxy.Forward(fmt.Sprintf("%s%s", config.USER_SERVICE, USER_CREATE_ROUTE)))
 }
