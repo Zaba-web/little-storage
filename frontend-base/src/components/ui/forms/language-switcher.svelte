bcc7f2d6e1ea764edfbc7fde07cfb2784984cf2f
Andriy Sotnyk <lodochnikkot@gmail.com>
2024-06-10 07:39:35 +0300

    add language switched component

---
+++ frontend-base/src/components/ui/forms/language-switcher.svelte
@@ -0,0 +1,68 @@
+<script>
+  import { __ } from "../../../utils/transalte";
+  import ukFlag from "../../../assets/icons/language/uk.svg";
+  import uaFlag from "../../../assets/icons/language/ua.svg";
+  import arrowDown from "../../../assets/icons/arrow_down.svg";
+  import { slide } from "svelte/transition";
+
+  $: currentLanguageCode = localStorage.getItem("languageCode") || "en";
+  $: expanded = false;
+
+  let languageList = {
+    en: {
+      name: __("English"),
+      image: ukFlag,
+    },
+    ua: {
+      name: __("Ukrainian"),
+      image: uaFlag,
+    },
+  };
+
+  $: currentLanguage = languageList[currentLanguageCode];
+
+  const changeLanguage = (code) => {
+    currentLanguage = languageList[code];
+    localStorage.setItem("languageCode", code);
+    expanded = false;
+  };
+</script>
+
+<div class="relative">
+  <div
+    class="flex gap-2 cursor-pointer"
+    on:click={() => (expanded = !expanded)}
+    role="button"
+  >
+    <img src={currentLanguage.image} alt={currentLanguage.name} class="w-4" />
+    <span
+      class="font-theme-default text-theme-gray font-medium flex items-center gap-2"
+    >
+      {currentLanguage.name}
+      <img src={arrowDown} alt="Expand" />
+    </span>
+  </div>
+  {#if expanded}
+    <div class="absolute" transition:slide>
+      {#each Object.keys(languageList) as language}
+        <div
+          class="flex gap-2 cursor-pointer mt-3"
+          on:click={() => {
+            changeLanguage(language);
+          }}
+        >
+          <img
+            src={languageList[language].image}
+            alt={languageList[language].name}
+            class="w-4"
+          />
+          <span
+            class="font-theme-default text-theme-gray hover:text-white font-medium flex items-center gap-2"
+          >
+            {languageList[language].name}
+          </span>
+        </div>
+      {/each}
+    </div>
+  {/if}
+</div>
--- frontend-base/src/pages/home.svelte
+++ frontend-base/src/pages/home.svelte
@@ -2,18 +2,24 @@
   import FullScreenContainer from "../components/ui/layout/full-screen-contianer.svelte";
   import { __ } from "../utils/transalte";
   import RegisterForm from "../components/ui/forms/registerForm.svelte";
+  import LanguageSwitcher from "../components/ui/forms/language-switcher.svelte";
 </script>
 
 <FullScreenContainer bgColor="theme-dark-50">
-  <div class="px-16 py-12 w-96 bg-theme-dark-100">
-    <h1 class="text-white font-theme-default font-regular text-xl">
-      {__("Request Membership")}
-    </h1>
-    <span class="text-theme-gray text-lg font-light">
-      {__("Do you want to join?")}
-    </span>
-    <div>
-      <RegisterForm />
+  <div>
+    <div class="flex justify-end mb-4">
+      <LanguageSwitcher />
+    </div>
+    <div class="px-16 py-12 w-96 bg-theme-dark-100">
+      <h1 class="text-white font-theme-default font-regular text-xl">
+        {__("Request Membership")}
+      </h1>
+      <span class="text-theme-gray text-lg font-light">
+        {__("Do you want to join?")}
+      </span>
+      <div>
+        <RegisterForm />
+      </div>
     </div>
   </div>
 </FullScreenContainer>
